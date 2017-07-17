32fdc169e67c7fcd9da3b35ae842d3cc61e89c7c
Unknown <rnalesa@gmail.com>
2017-07-17 11:34:27 +0200

    fix bugs, add recursive function to check for all children

--- node_attributes/node_attributes.py
+++ node_attributes/node_attributes.py
@@ -13,7 +13,7 @@ def find_node(data, node_label):
 
 
 # Find a specific label by node
-def find_label(data, node_id):
+def find_label(node_id):
     for node in data["nodes"]:
         if "id" in node:
             if node_id in node["id"]:
@@ -23,8 +23,9 @@ def find_label(data, node_id):
                     return "No name"
 
 
+# Tested
 # Generate tree of the callgraph
-def generate_tree(entry_node, data, root, visited=None):
+def generate_tree(entry_node, root, visited=None):
     if visited is None:
         visited = set()
     for link in data["links"]:
@@ -32,21 +33,26 @@ def generate_tree(entry_node, data, root, visited=None):
             key = hash(entry_node + link["source"] + link["target"])
             if key not in visited:
                 visited.add(key)
-                name = find_label(data, link["target"])
+                name = find_label(link["target"])
                 new_node = Node(name, parent=root)
-                generate_tree(link["target"], data, new_node, visited)
+                generate_tree(link["target"], new_node, visited)
     return root
 
 
+# Tested
 # The degree of a node is defined as the number of its neighboring edges
-def node_degree(node, data):
+def node_degree(node):
+    visited = set()
     counter_out = 0
     counter_in = 0
     for link in data["links"]:
-        if node in link["source"]:
-            counter_out += 1
-        if node in link["target"]:
-            counter_in += 1
+        key = hash(link["source"] + link["target"])
+        if key not in visited:
+            if node in link["source"]:
+                counter_out += 1
+            if node in link["target"]:
+                counter_in += 1
+            visited.add(key)
     return counter_out, counter_in, counter_out + counter_in
 
 
@@ -59,53 +65,67 @@ def node_path_length(tree, node_name):
     start = '*'
     for i in range(tree.height):
         node = resolver.glob(tree, start + node_name)
-        if node != []:
-            for child in node[0].children:
-                total += child.height
-                children_number += 1
-            if children_number != 0:
-                total = total / children_number
+        if node:
+            node[0].parent = None
+            result = get_all_children(node[0], total, children_number, True)
+            total += result[0]
+            children_number += result[1]
         else:
             start += '/*'
+    if children_number != 0:
+        total = total / children_number
     return total
 
 
+# Looks for the shortest path to the requested node
+def get_all_children(node, total, children_number, isRoot=False):
+    for child in node.children:
+        if child.children:
+            result = get_all_children(child, total, children_number)
+            total += result[0]
+            children_number += result[1]
+        else:
+            total += child.depth
+            children_number += 1
+    if isRoot:
+        return total, children_number
+    else:
+        return total, children_number
+
+
+# Tested
 # For a node u, the clustering coefficient c(u) represents the likelihood that any two neighbors of u are connected.
 # Step 1: Get list, L, of all connected (sources or targets, anything) nodes to "node". 
 # Step 2: For all pairs (a, b) of nodes in L:
 #   Step 2a: If a!=b and a and b are connected (a->b or b->a), then add to clustering coefficient
 def clustering_coefficient(list, degree):
+    visited = set()
     triangles = 0
     ru = ((degree ** 2) - degree) / 2
-    for a in list:
-        for b in list:
-            if a['source'] != b['source']:
-                if a['target'] == b['source']:
+    for related_node in list:
+        for link in data["links"]:
+            key = hash(related_node + link["source"] + link["target"])
+            if key not in visited:
+                if (related_node in link["source"]) and (link["target"] in list) and (link["source"] != link["target"]):
+                    visited.add(key)
                     triangles += 1
-                if b['target'] == a['source']:
-                    triangles += 1
+                    # print("from:", find_label(link["source"]), "to:", find_label(link["target"]))
     return triangles / ru if ru != 0 else 0
 
 
-def generate_connected_list(requested_node, data):
-    visited = set()
+# Tested
+def generate_connected_list(requested_node):
     list_nodes = []
-    final_list = []
     for link in data["links"]:
         if requested_node in link["source"]:
             list_nodes.append(link['target'])
         if requested_node in link["target"]:
             list_nodes.append(link['source'])
-    for related_node in list_nodes:
-        for link in data["links"]:
-            if related_node in link["source"]:
-                key = hash(entry_node + link["source"] + link["target"])
-                if key not in visited:
-                    visited.add(key)
-                    final_list.append(link)
-    return final_list
+    set_nodes = list(set(list_nodes))
+    return set_nodes
 
 
+# Tested
 # Distance from the node to the entry node.
 def distance_to_interface(tree, node_name):
     resolver = Resolver('name')
@@ -118,28 +138,12 @@ def distance_to_interface(tree, node_name):
             start += '/*'
 
 
-# -- NO LONGER IN DEV --
-# Might not be useful for directed graph -- eccentricity of most nodes is infinity
-# The eccentricity of a node u is defined as e(u) = max{d(u, v) : v ∈ V },
-# where the distance d(u, v) is the length of the shortest path from u to v.
-def effective_eccentricity(node, data):
-    return
-
-
-# -- NO LONGER IN DEV --
-# Might not be useful for graphs with unique labels. 
-# We define the impurity degree of a node u as: ImpurityDeg(u) = |L(v) : v ∈ N(u), L(u) 6= L(v)|
-# where L(u) is the label, and N(u) is the neighborhood of (the nodes adjacent to) node u.
-def neighborhood_impurity(node, data):
-    return
-
-
 # Print results out.
 def print_results():
     requested_node = find_node(data, node_name)
-    connected_list = generate_connected_list(requested_node, data)
+    connected_list = generate_connected_list(requested_node)
 
-    degree = node_degree(requested_node, data)
+    degree = node_degree(requested_node)
     print("\nThe results for node \"" + node_name + "\" with interface \"" + interface + "\" are:\n")
     print("\tNode degree (out, in, total):", degree)
     print("\tNode distance to interface:", distance_to_interface(tree, node_name))
@@ -156,7 +160,7 @@ else:
     interface = sys.argv[3] if len(sys.argv) > 3 else "external node"
     entry_node = find_node(data, interface)
     root = Node(interface)
-    tree = generate_tree(entry_node, data, root)
+    tree = generate_tree(entry_node, root)
     RenderTreeGraph(tree).to_picture("Callgraph.png")
 
     for pre, fill, node in RenderTree(tree):
