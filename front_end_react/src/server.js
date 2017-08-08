3c45e1508500d5b769ee1bc9f6d222b0aeb8beb4
Unknown <rnalesa@gmail.com>
2017-08-08 14:00:14 +0200

    add frontend

---
+++ front_end_react/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './app';
+import template from './template';
+
+const server = express();
+
+server.use('/assets', express.static('assets'));
+
+server.get('/', (req, res) => {
+  const isMobile = true;
+  const initialState = { isMobile };
+  const appString = renderToString(<App {...initialState} />);
+
+  res.send(template({
+    body: appString,
+    title: 'Front-end',
+    initialState: JSON.stringify(initialState)
+  }));
+});
+
+let port = 8000;
+server.listen(port);
+console.log('listening on port:', port);
