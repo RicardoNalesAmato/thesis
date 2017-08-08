e93c0d1e1f14300b362bec7c600caa74a672c296
Unknown <rnalesa@gmail.com>
2017-08-08 11:30:33 +0200

    finish correlation

--- node_cvss_correlation/node_cvss_correlation.py
+++ node_cvss_correlation/node_cvss_correlation.py
@@ -1,18 +1,109 @@
 import sys
 import json
 import numpy
+import os
+
+
+# We need only the values for CVSS3 scores of the resulting array NODEATTRIBUTES + CVSS3
+def prep_data(function_data):
+    global cvss3_data
+    result = []
+    for key, value in sorted(function_data.items()):
+        if key == 'clustering_coefficient':
+            result.append(value)
+        elif key == 'cvss3':
+            cvss3_data = value
+        elif key == 'distance_to_interface':
+            if value is None:
+                result.append(0)
+            else:
+                result.append(1)
+        elif key == 'macke_bug_chain_length':
+            result.append(value)
+        elif key == 'macke_vulnerabilities_found':
+            result.append(value)
+        elif key == 'node_degree':
+            result.append(value[2])
+        elif key == 'node_path_length':
+            result.append(value)
+
+    # Mapping CVSS3 values
+    for cvss3_entry_key, cvss3_entry_data in sorted(cvss3_data.items()):
+        if cvss3_entry_key == 'attackVector':
+            if cvss3_entry_data == 'NETWORK':
+                result.append(0)
+            elif cvss3_entry_data == 'ADJACENT':
+                result.append(1)
+            elif cvss3_entry_data == 'LOCAL':
+                result.append(2)
+            elif cvss3_entry_data == 'PHYSICAL':
+                result.append(3)
+        elif cvss3_entry_key == 'attackComplexity':
+            if cvss3_entry_data == 'LOW':
+                result.append(0)
+            elif cvss3_entry_data == 'HIGH':
+                result.append(1)
+        elif cvss3_entry_key == 'privilegesRequired':
+            if cvss3_entry_data == 'NONE':
+                result.append(0)
+            elif cvss3_entry_data == 'LOW':
+                result.append(1)
+            elif cvss3_entry_data == 'HIGH':
+                result.append(2)
+        elif cvss3_entry_key == 'userInteraction':
+            if cvss3_entry_data == 'NONE':
+                result.append(0)
+            elif cvss3_entry_data == 'REQUIRED':
+                result.append(1)
+        elif cvss3_entry_key == 'scope':
+            if cvss3_entry_data == 'UNCHANGED':
+                result.append(0)
+            elif cvss3_entry_data == 'CHANGED':
+                result.append(1)
+        elif cvss3_entry_key == 'confidentialityImpact':
+            if cvss3_entry_data == 'NONE':
+                result.append(0)
+            elif cvss3_entry_data == 'LOW':
+                result.append(1)
+            elif cvss3_entry_data == 'HIGH':
+                result.append(2)
+        elif cvss3_entry_key == 'integrityImpact':
+            if cvss3_entry_data == 'NONE':
+                result.append(0)
+            elif cvss3_entry_data == 'LOW':
+                result.append(1)
+            elif cvss3_entry_data == 'HIGH':
+                result.append(2)
+        elif cvss3_entry_key == 'availabilityImpact':
+            if cvss3_entry_data == 'NONE':
+                result.append(0)
+            elif cvss3_entry_data == 'LOW':
+                result.append(1)
+            elif cvss3_entry_data == 'HIGH':
+                result.append(2)
+    return result
+
 
 # Main
 if len(sys.argv) < 1:
-    sys.stderr.write("Syntax : python3 %s attributes_file.dot.json\n" % sys.argv[0])
+    sys.stderr.write('Syntax : python3 %s /node_attributes_directory\n' % sys.argv[0])
 else:
-    with open(sys.argv[1]) as data_file:
-        data = json.load(data_file)
-    for function_name, function_data in data.items():
-        if function_data["faulty"]:
-            covariance = numpy.cov([[1, 2, 3, 4, 5], [5, 65, 345, 2, 7]], rowvar=False)
-            correlation = numpy.corrcoef([[1, 2, 3, 4, 5], [5, 65, 345, 2, 7]], rowvar=False)
-            print(covariance)
-            print(correlation)
+    files = os.listdir(sys.argv[1])
+    data = []
+    structure_data = []
+    for file in files:
+        with open(os.path.join(sys.argv[1], file)) as data_file:
+            all_functions = json.load(data_file)
+            for key, value in all_functions.items():
+                if value['faulty'] is True:
+                    data.append(value)
 
-# We need only the values for CVSS3 scores of the resulting array NODEATTRIBUTES + CVSS3
+    for function_data in data:
+        structure_data.append(prep_data(function_data))
+
+    covariance = numpy.cov(structure_data, rowvar=False)
+    correlation = numpy.corrcoef(structure_data, rowvar=False)
+    print('Covariance:\n', covariance)
+    print('Correlation of CVSS3 Scores:\n')
+    for i in range(5, 14):
+        print(correlation[i])
