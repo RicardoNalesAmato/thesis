8be00a73a17ed1ac149674bcb241cc4d6f63f8a5
Unknown <rnalesa@gmail.com>
2017-09-04 12:06:50 +0200

    new structure

--- predictions/predictions.py
+++ predictions/predictions.py
@@ -3,25 +3,26 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 def gaussian_learner(X, y):
-    print('GAUSSIAN NAIVE BAYES:')
     gnb = GaussianNB()
     gnb.fit(X, y)
-    print(gnb.predict([[2, 3]]))
+    return gnb
 
 
 def random_forest_learner(X, y):
-    print('RANDOM FOREST:')
     clf = RandomForestClassifier(random_state=0)
     clf.fit(X, y)
-    print(clf.feature_importances_)
-    print(clf.predict([[2, 3]]))
+    return clf
 
 # Main
-
 # Attributes - TODO Ask Saahil how to structure them
 X = [[32, 43], [2, 3], [435, 546]]
 # Values - TODO Ask Saahil how to structure them
 y = [0, 2, 1]
 
-gaussian_learner(X, y)
-random_forest_learner(X, y)
+gaussian_test_learner = gaussian_learner(X, y)
+rf_test_learner = random_forest_learner(X, y)
+
+print('GAUSSIAN NAIVE BAYES:')
+print(gaussian_test_learner.predict([[2,3]]))
+print('RANDOM FOREST:')
+print(rf_test_learner.predict([[2,3]]))
